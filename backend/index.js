e2f763b514c3f38e1906f6b31a7bd2b26e9ddf5f
Minhaz-039 <minhazreza3243@gmail.com>
2025-01-09 05:24:04 +0600

    Added a lot of files from react

--- backend/index.js
+++ backend/index.js
@@ -1,27 +1,37 @@
 import path from 'path';
 import express from 'express';
-import dotenv from 'dotenv';  
+import dotenv from 'dotenv';
 import cookieParser from 'cookie-parser';
+import cors from 'cors';  // Import CORS
 
-
-//Utils
+// Utils
 import connectDB from './config/db.js';
+import userRoutes from './routes/userRoutes.js';
 
 dotenv.config();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 6000;
 
 connectDB();
 
 const app = express();
 
+// CORS Setup
+const corsOptions = {
+  origin: 'http://localhost:5173',  // Allow requests from your frontend
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,  // Allow cookies to be sent
+};
+
+app.use(cors(corsOptions));  // Apply the CORS middleware
+
+// Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.get('/', (req, res) => {
-    res.send('API is running....');
-});
+// Routes
+app.use('/api/users', userRoutes);
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-})
+  console.log(`Server is running on port ${port}`);
+});
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,52 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import {createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {Route , RouterProvider , createRoutesFromElements} from 'react-router'
+import { createBrowserRouter } from 'react-router'
+import { Provider } from 'react-redux'
+import store from './redux/store'
+import Profile from './pages/User/Profile.jsx'
+import AdminRoutes from './pages/Admin/AdminRoutes.jsx'
+
+
+//Private Routes
+import PrivateRoutes from './components/PrivateRoutes.jsx'
+
+
+// Auth
+import LogIn from './pages/Auth/LogIn.jsx'
+import Register from './pages/Auth/Register.jsx'
+import UserList from './pages/Admin/UserList.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />} >
+
+
+      <Route path='/login' element={<LogIn />} />
+      <Route path='/register' element={<Register />} />
+
+
+      <Route path='' element={<PrivateRoutes />} >
+        <Route path='/profile' element={<Profile />} />
+      </Route>
+
+      {/* Admin Routes */}
+      <Route path='/admin' element={<AdminRoutes />} >
+        <Route path="userlist" element={<UserList />} />
+      </Route>
+
+
+    </Route>
+  )
+);
+
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <Provider store={store}>
+    <RouterProvider router={router}/>
+  </Provider>
+    
+
 )
